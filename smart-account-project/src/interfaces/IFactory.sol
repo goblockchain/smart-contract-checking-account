9e07592b7c053534bdedd89cd90a5aaf4809f0d3
Caio Barros <caiohenriquesa1@gmail.com>
2023-11-16 16:54:53 -0300

    architecture & interfaces for erc20-like allocations

--- smart-account-project/src/interfaces/IFactory.sol
+++ smart-account-project/src/interfaces/IFactory.sol
@@ -2,21 +2,9 @@
 pragma solidity ^0.8.0;
 
 interface IFactory {
-    /**
-     * company's name,
-     * minAllocation by user,
-     * paymentDay(s),
-     * ERC20 accepted tokens,
-     * ERC721 accepted tokens,
-     * ERC1155 accepted tokens,
-     * accepts erc20, (flag to avoid waste of gas)
-     * accepts erc721, (flag to avoid waste of gas)
-     * accepts erc1155, (flag to avoid waste of gas)
-     * percentage upon allocation
-     */
-
-    /// callable only by goBlockchain. Have an id for each company. Sort permitted tokens to place them correctly in a tokenIndex mapping.
+    /// @notice callable only by goBlockchain. Have an id for each company. Sort permitted tokens to place them correctly in a tokenIndex mapping. address(0) must be the token of ID 0, so that we can avoid users passing in address(0) tokens.
     /// @param company company's name
+    /// @param companyAdmins company wallets with access to modify state of the Smart Account.
     /// @param minAllocation min allowed allocation by user
     /// @param acceptERC20Tokens flag to allow/disallow erc20s allocations
     /// @param permittedERC20Tokens permitted erc20 tokens
@@ -24,10 +12,12 @@ interface IFactory {
     /// @param permittedERC721Tokens permitted erc721 tokens
     /// @param acceptERC1155Tokens flag to allow/disallow erc1155s allocations
     /// @param permittedERC1155Tokens permitted erc1155 tokens
-    /// @param percentageFromAllocation percentage of credit given to user.
+    /// @param percentageFromAllocation percentage of allowcation given to user as credit.
+    /// @param paymentTokens tokens allowed to be received as a payment for debt.
     /// @return companyId company's ID generated upon creation
     function create(
         string calldata company,
+        address[] calldata companyAdmins,
         uint minAllocation,
         bool acceptERC20Tokens,
         address[] calldata permittedERC20Tokens,
@@ -35,14 +25,20 @@ interface IFactory {
         address[] calldata permittedERC721Tokens,
         bool acceptERC1155Tokens,
         address[] calldata permittedERC1155Tokens,
-        uint percentageFromAllocation
+        uint percentageFromAllocation,
+        address[] calldata paymentTokens
     ) external returns (uint companyId);
 
-    /// public mapping showing companies' status.
+    /// @notice public mapping showing companies' status.
     /// @param companyId company whose status is being checked.
     function active(uint companyId) external view returns (bool);
 
-    /// Marks company as inactive and pauses Smart Account company's contract. Refunds should be given to users, if any. Callable by goBlockchain only.
+    /// @notice Marks company as inactive and pauses Smart Account company's contract. Refunds should be given to users, if any. Callable by goBlockchain only.
     /// @param companyId ID of company to inactivate.
     function deactivate(uint companyId, bool refund) external returns (bool);
+
+    /// @notice function callable by goBlockchain to withdraw any tokens directly transferred to this contract by accident. If token to be withdrawn is the zero address, withdraw ether from contract.
+    /// @param token token to be withdrawn.
+    /// @param to to whom it should be given to, possibly being the user who sent it by accident.
+    function skim(address token, address to) external returns (bool);
 }
--- smart-account-project/src/interfaces/ISmartAccount.sol
+++ smart-account-project/src/interfaces/ISmartAccount.sol
@@ -2,71 +2,117 @@
 pragma solidity ^0.8.0;
 
 interface ISmartAccount {
-    /// company's name
+    /// @notice company's name.
     function name() external view returns (string memory);
 
-    /// callable by factory. It sets the config options for the Smart Account.
-    function init() external;
+    /// @notice checks whether `account` is an authorized for the company
+    /// @param account to verify
+    function auth(address account) external view returns (bool);
 
-    /// public mapping of tokenID -> token address in SmartAccount. Addresses are put into tokenIndex after sorting them in factory
-    function tokenIndex(uint tokenIndex) external view returns (address);
+    /// @notice mapping of user's current credit (>0) or debt (<0).
+    /// @param usr user for credit or debt to be retrieved.
+    function credit(address usr) external view returns (int);
 
-    /// used by user to allocate funds and get debts
-    function allocate() external;
+    /// @notice mapping of user's max credit calculated from user's allocation. Consequentially, it is also the maxDebt for a user.
+    /// @param usr user for which max credit is retrieved.
+    function maxCredit(address usr) external view returns (uint256);
 
-    /// used by company to allocate with permit of user
-    function allocateWithPermit(
-        uint tokenIndex,
+    /// @notice it increases each time the user `payback()`. It diminishes when user doesn't inccurs debt multiple times. It can be used futurely for giving a usr specific rewards according to his/her score.
+    function score(address usr) external view returns (int);
+
+    /// @notice callable by factory. It sets the config options for the Smart Account.
+    function init(
+        uint companyId,
+        string calldata company,
+        address[] calldata companyAdmins,
+        uint minAllocation,
+        bool acceptsERC20Tokens,
+        address[] calldata sortedPermittedERC20Tokens,
+        bool acceptsERC721Tokens,
+        address[] calldata sortedPermittedERC721Tokens,
+        bool acceptsERC1155Tokens,
+        address[] calldata sortedPermittedERC1155Tokens,
+        uint percentageFromAllocation,
+        address[] calldata paymentTokens
+    ) external returns (bool);
+
+    /// @notice public mapping of tokenID -> token address in SmartAccount. Addresses are put into tokenIndex after sorting them in factory. These are the allowed tokens to be used as a allocation.
+    function tokenIndex(uint256 tokenIndex) external view returns (address);
+
+    /// @notice used by user to allocate funds and get debts. Make it have a `lock` modifier.
+    function allocateERC20(uint256 tokenIndex, uint256 amount) external;
+
+    /// @notice it permits a user to give his credits to another user, but the debt will be calculated against the delegator, not the delegatee.
+    function allocateERC20Delegate(
+        uint256 tokenIndex,
+        uint256 amount,
+        address to
+    ) external;
+
+    /// Main function called by the company (or goblockchain?) to pull user funds to this contract and give him credit. `nonReentrant` function, but allowed to be called in different times for the same user to give him compound credit.
+    /// @param tokenIndex token to allocate
+    /// @param amount amount of token to allocate
+    /// @param deadline deadline for token to be allocated.
+    /// @param includesNonce does the token include a nonce (e.g. DAI)
+    /// @param nonce tx's nonce for token allocation, if any.
+    /// @param v sig param
+    /// @param r sig param
+    /// @param s sig param
+    function allocateERC20WithPermit(
+        uint256 tokenIndex,
         uint256 amount,
         uint256 deadline,
         bool includesNonce, // some tokens, like DAI seem to have a nonce in their permit functions.
-        uint nonce,
+        uint256 nonce,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) external;
 
-    /// only to be called by company's contract address.
-    function borrow() external;
+    /// @notice Main function called by the company (or goblockchain?) to pull user funds to this contract and give him credit. `nonReentrant` function, but allowed to be called in different times for the same user to give him compound credit.
+    /// @param tokenIndexes tokens the user want to allocate to receive credit. Must be in inside the permitted tokens addresses chosen by the company.
+    /// @param amounts amounts of each token to be deposited.
+    /// @param deadlines deadlines for the transfers to happen.
+    /// @param includesNonce whether tokens use includes nonces
+    /// @param nonces nonces for tokens. Choose an arbitrary number those who haven't nonces
+    /// @param v sig param
+    /// @param r sig param
+    /// @param s sig param
+    function batchAllocateERC20WithPermit(
+        uint256[] calldata tokenIndexes,
+        uint256[] calldata amounts,
+        uint256[] calldata deadlines,
+        bool[] calldata includesNonce, // some tokens, like DAI seem to have a nonce in their permit functions.
+        uint256[] calldata nonces,
+        uint8[] calldata v,
+        bytes32[] calldata r,
+        bytes32[] calldata s
+    ) external;
 
-    /// used to pay debt
-    function payback() external;
+    /// @notice only to be called by company's contract address. It's called when company updates the states of the user according to their usage of the credit card off-chain. It can be called in batches to avoid block-max-gas-limit revert error in the chain being used.
+    /// @param users user for state to be updated. Must be already a user whose allocated tokens in the SmartAccount.
+    /// @param amounts debt or credit of user in a given time. It is used to update the `credit` mapping.
+    function update(address[] memory users, int[] calldata amounts) external;
 
-    /// used by user to cease his/her participation in the protocol
-    function cease() external;
+    /// @notice used by user to cease his/her participation in the protocol
+    function cease(uint8 v, bytes32 r, bytes32 s, bytes memory data) external;
 
-    /// used by company to cease user's participation in the protocol
-    function ceaseWithPermit(address usr) external;
+    /// @notice used by company to cease a user's participation in the protocol.
+    function cease(address usr) external;
 
-    /// returns excess credit for the user in a given month
-    function excess(address usr) external view returns (uint256);
-
-    /// returns current debt for the user in a given month
-    function debt(address usr) external view returns (uint256);
-
-    /// pause companies' mains functionalities. Callable only by Factory on deactivate.
+    /// @notice pause companies' mains functionalities. Callable only by Factory on deactivate.
     function pause() external;
 
-    /// unpauses company's mains functionalities. Callable only by Factory on activate.
+    /// @notice unpauses company's mains functionalities. Callable only by Factory on activate.
     function unpause() external;
+
+    /// @notice function callable by company to withdraw any tokens directly transferred to this contract by accident or leftovers from solidity's rounding arithmetic. If token to be withdrawn is the zero address, withdraw ether from contract.
+    /// @param token token to be withdrawn.
+    /// @param to to whom it should be given to, possibly being the user who sent it by accident.
+    function skim(address token, address to) external returns (bool);
+
+    /// @notice We still need to decide how to penalize the user on-chain. This is fundamental to how the protocol will work. If the user does not fear being penalized, (s)he won't have any fear of incurring debt sequentially. One of the ways to do it on chain is to dimish user's score.
+    /// @param users users to be punished.
+    /// @param amounts amounts in which each user is to be punished.
+    function punish(address[] calldata users, uint amounts) external;
 }
-
-/**
- * Flow inside smart account:
- * user.allocate() or company allocateWithPermit()
- *
- */
-
-/*
-    How will the contract flow work?
-
-    goBlockchain(+) -> Factory -> Smart Account of Company
-    goBlockchain(-) -> Factory -> Smart Account of Company
-
-    User calldata(+) -> allocate -> Smart Account of Company
-    User borrow(-) -> Company's contract -> Smart Account of Company
-    User payback(+) -> Smart Account of Company
-    User withdraw(-) -> Company's contract -> Smart Account of Company
-
-    Make a contract for the company where it can list different accounts on it and these accounts can be used to call the SmartAccount contract. (Company's contract) 
-*/
