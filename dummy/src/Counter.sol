a071929ee7911a8f85c28f54a2503c6e05eb6680
Caio Barros <caiohenriquesa1@gmail.com>
2023-11-24 01:24:58 -0300

    triyng to make meta-tx work, but it's HARD!

--- dummy/src/Counter.sol
+++ dummy/src/Counter.sol
@@ -3,16 +3,24 @@ pragma solidity ^0.8.13;
 
 import {ERC2771Context} from "../lib/openzeppelin-contracts/contracts/metatx/ERC2771Context.sol";
 import {ERC2771Forwarder} from "../lib/openzeppelin-contracts/contracts/metatx/ERC2771Forwarder.sol";
+import "../lib/forge-std/src/console.sol";
+import "../lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 
-contract Counter is ERC2771Forwarder, ERC2771Context{
+contract Counter is ERC2771Forwarder, ERC2771Context {
+    using ECDSA for bytes32;
     uint256 public number;
 
-    constructor() ERC2771Forwarder("DummyProtocol") ERC2771Context(address(this)) {
-
-    }
+    constructor()
+        ERC2771Forwarder("DummyProtocol")
+        ERC2771Context(address(this))
+    {}
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+    }
+
+    function hashTypedDataV4(bytes32 param) public view returns (bytes32) {
+        return _hashTypedDataV4(param);
     }
 
     function increment() public {
@@ -30,8 +38,9 @@ contract Counter is ERC2771Forwarder, ERC2771Context{
     ) external payable {
         //ForwardRequestData { address from; address to; uint256 value; uint256 gas; uint48 deadline; bytes data; bytes signature;}
         // ForwardRequestData memory metaTx = ForwardRequestData({from: _userAddress, to: _to, value: _value, gas: _gas, deadline: block.timestamp + 1 days, data: _functionData, signature: _signature});
-        
-        // this verifies whether the user equals the signer. 
+        // NOTE: I don't have to implement the nonce. It seems everything is handled by the `ERC2771Forwarder`
+
+        // this verifies whether the user equals the signer.
         require(verify(metaTx), "WRONG#SIGNER");
         //require(_verify(_userAddress, metaTx, _signature), "NativeForwardRequest#executeForwardRequest: SIGNER_AND_SIGNATURE_DO_NOT_MATCH");
 
@@ -41,33 +50,32 @@ contract Counter is ERC2771Forwarder, ERC2771Context{
         // TODO: instead of below, use the `execute` function.
         execute(metaTx);
         //(bool success, bytes memory returnData) = address(this).call{value: msg.value}(abi.encodePacked(_functionData, _userAddress));
-
-        /*
-        // Bubble up error based on https://ethereum.stackexchange.com/a/83577
-        if (!success) {
-            assembly {
-                // Slice the sighash.
-                returnData := add(returnData, 0x04)
-            }
-
-            revert(abi.decode(returnData, (string)));
-        }
-        return returnData;
-        */
-        
     }
 
-    /*
-    function _verify(
-        address _signer,
-        ForwardRequestData memory _metaTx,
-        bytes calldata _signature
-    ) private view returns (bool) {
-        bytes32 structHash = keccak256(abi.encode(META_TRANSACTION_TYPEHASH, _metaTx.nonce, _metaTx.from, keccak256(_metaTx.functionData)));
-        bytes32 typedDataHash = _hashTypedDataV4(structHash);
+    function generateMessage(
+        address _from,
+        address _to, // this contract
+        uint256 _value, // 0
+        uint _gas, //300000
+        bytes memory _data // 0x3fb5c1cb0000000000000000000000000000000000000000000000000000000000000005 (setNumber(5))
+    ) public view returns (bytes32, uint, uint) {
+        uint deadline = block.timestamp + 1 days;
 
-        return _signer == ECDSAUpgradeable.recover(typedDataHash, _signature);
+        return (
+            keccak256(
+                abi.encode(
+                    _FORWARD_REQUEST_TYPEHASH,
+                    _from,
+                    _to,
+                    _value,
+                    _gas,
+                    nonces(_from),
+                    deadline,
+                    _data
+                )
+            ),
+            deadline,
+            nonces(_from)
+        );
     }
-    */
-
 }
--- dummy/test/Counter.t.sol
+++ dummy/test/Counter.t.sol
@@ -2,23 +2,137 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Counter, ERC2771Forwarder} from "../src/Counter.sol";
+import {ECDSA} from "../lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+
+// import "../lib/openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
 
 contract CounterTest is Test {
+    using ECDSA for bytes32;
     Counter public counter;
+    address from;
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+    /**
+     * User variables
+     */
+    uint FROM_PRIV_KEY;
+    bytes32 constant _FORWARD_REQUEST_TYPEHASH =
+        keccak256(
+            "ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,uint48 deadline,bytes data)"
+        );
 
     function setUp() public {
+        vm.deal(address(10), 10 ether);
+        vm.startPrank(address(10));
         counter = new Counter();
+        FROM_PRIV_KEY = vm.envUint("PRIV");
+        from = vm.addr(FROM_PRIV_KEY);
+        emit log_address(from);
         counter.setNumber(0);
+
+        vm.deal(from, 10 ether);
+        vm.startPrank(from);
+
+        vm.stopPrank();
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    function test_final() public {
+        vm.startPrank(address(this));
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        // Prepare the message hash using the same hashing method
+        bytes32 messageHash = keccak256(
+            abi.encode(
+                _FORWARD_REQUEST_TYPEHASH,
+                from,
+                address(counter),
+                0,
+                500000,
+                uint48(block.timestamp + 86400),
+                keccak256(abi.encodePacked(hex"3fb5c1cb", abi.encode(uint(5))))
+            )
+        );
+
+        (uint8 v_, bytes32 r_, bytes32 s_) = vm.sign(
+            FROM_PRIV_KEY,
+            messageHash
+        );
+        bytes memory _signature1 = abi.encodePacked(r_, s_, v_);
+
+        //----------------------------------------------------------------------
+        /*
+         * ```solidity
+         * bytes32 digest = _hashTypedDataV4(
+         * keccak256(
+         * abi.encode(
+         * keccak256(
+         * "Mail(address to,string contents)"
+         * ), // keccak256
+         *     mailTo,
+         *     keccak256(bytes(mailContents))
+         * ) // encode
+         * ) // keccak
+         * ); // hashTypedDataV4
+         * address signer = ECDSA.recover(digest, signature);
+         * ```
+         */
+        // NOTE: trying to follow above's example:
+        bytes memory _data = abi.encodePacked(
+            keccak256(abi.encodePacked(hex"3fb5c1cb", abi.encode(uint(5))))
+        );
+
+        bytes32 digest = counter.hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    _FORWARD_REQUEST_TYPEHASH,
+                    from,
+                    address(counter),
+                    0,
+                    500000,
+                    uint48(block.timestamp + 86400),
+                    _data,
+                    _signature1
+                )
+            )
+        );
+
+        (uint8 vv, bytes32 rr, bytes32 ss) = vm.sign(FROM_PRIV_KEY, digest);
+
+        bytes memory _signature2 = abi.encodePacked(rr, ss, vv);
+        emit log_named_bytes("sig", _signature2);
+
+        counter.verify(
+            ERC2771Forwarder.ForwardRequestData({
+                from: from,
+                to: address(counter),
+                value: 0, // Set your desired value
+                gas: 500000, // You can set your desired gas value
+                deadline: uint48(block.timestamp + 86400), // Set your desired deadline
+                data: _data,
+                signature: _signature2
+            })
+        );
+
+        // works
+        (address recovered, , ) = ECDSA.tryRecover(messageHash, _signature1);
+        emit log_address(recovered);
     }
 }
+
+/**
+ *          bytes32 digest = _hashTypedDataV4(keccak256(
+                abi.encode(
+                    _FORWARD_REQUEST_TYPEHASH,
+                    request.from,
+                    request.to,
+                    request.value,
+                    request.gas,
+                    nonces(request.from),
+                    request.deadline,
+                    keccak256(request.data)
+                )
+            ))
+
+            recover from digest + signature.
+ */
